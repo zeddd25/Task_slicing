2ae3d9f3b171328e1bdda579ae67fb5827f0f939
zeddd25 <muhamaddzilliassajad@gmail.com>
2023-04-05 11:45:14 +0700

    Update my slicing

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import instance from "../api";
 
 const Register = () => {
